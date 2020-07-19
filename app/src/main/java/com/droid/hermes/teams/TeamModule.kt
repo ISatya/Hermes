08f3ef12ec5616a6d2e5c39b1f8840620c898589
Isatya <7152029+ISatya@users.noreply.github.com>
2020-07-19 13:26:29 -0400

    - Create navigation graph   - Add team search as start destination

---
+++ app/src/main/java/com/droid/hermes/teams/TeamModule.kt
@@ -0,0 +1,7 @@
+package com.droid.hermes.teams
+
+/**
+ * Created on 19/07/20.
+ */
+class TeamModule {
+}
---
+++ app/src/main/java/com/droid/hermes/teams/presentation/teamSearch/TeamSearchFragment.kt
@@ -0,0 +1,9 @@
+package com.droid.hermes.teams.presentation.teamSearch
+
+import androidx.fragment.app.Fragment
+import com.droid.hermes.R
+
+/**
+ * Created on 19/07/20.
+ */
+class TeamSearchFragment : Fragment(R.layout.fragment_team_search)
