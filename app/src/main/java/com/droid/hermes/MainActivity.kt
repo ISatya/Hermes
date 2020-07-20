2003f7cc510cb4dcf6c7f64600e77093b1f2d997
Isatya <7152029+ISatya@users.noreply.github.com>
2020-07-19 21:41:42 -0400

    - Mark activity as @AndroidEntryPoint for hilt injection - setup action bar with nav controller

--- app/src/main/java/com/droid/hermes/MainActivity.kt
+++ app/src/main/java/com/droid/hermes/MainActivity.kt
@@ -1,5 +1,21 @@
 package com.droid.hermes
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity(R.layout.activity_main){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Setting up a back button
+        val navController: NavController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+}
